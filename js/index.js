fdf1590aa1c259de750bd59d064797160fc73a32
remberq <rember.out@gmail.com>
2022-05-03 22:34:50 +0300

    finall release

--- js/index.js
+++ js/index.js
@@ -1,62 +1,61 @@
+const swipers = document.querySelector('.swiper')
+
 const brand = document.querySelector('.hero__brand-list')
 const brandList = brand.querySelector('.hero__brand-items')
-const brandItem = brandList.querySelectorAll('.hero__brand-item')
-const swipers = document.querySelector('.swiper')
-const btnLink = document.querySelector('.hero__link-text')
-const btnImg = document.querySelector('.hero__show-link')
-const test = document.querySelectorAll('.hero__brand-item:nth-child(n+7)')
-const test2 = document.querySelectorAll('.hero__brand-item:nth-child(n+9)')
+
+const btnImg = brand.querySelector('.hero__show-link')
+const btnLink = btnImg.querySelector('.hero__link-text')
+
+// Получаем ширину экрана
+let width = window.innerWidth  
 
 
-window.onresize = () => {
-    const width = window.innerWidth
-    if (width < 420) {
-        brand.classList.add('hidden')
-        swipers.classList.remove('hidden')
+const changeButton = () => {
+    if (btnLink.textContent === 'Скрыть') {
+        btnLink.textContent = 'Показать'
+        btnImg.classList.remove('change')
+        brandList.style.overflow = 'hidden'
+        brandList.style.height = '176px'
+        
     } else {
-        brand.classList.remove('hidden')
-        swipers.classList.add('hidden')
+        btnLink.textContent = 'Скрыть'
+        btnImg.classList.add('change')
+        brandList.style.overflow = 'visible'
+        brandList.style.height = '100%'
     }
 }
 
 
+if (width <= 470) {
+    // Включаем свайпер
+    brand.classList.add('hidden')
+    swipers.classList.remove('hidden')
 
-btnLink.addEventListener('click', () => {
-    if (window.matchMedia("(max-width: 768px) and (min-width: 321px)").matches) {
-        if (btnLink.textContent === 'Скрыть') {
-            for (let item of test) {
-                item.classList.add('hidden')
-            }
-            btnLink.textContent = 'Показать'
-            btnImg.classList.add('change')
-            
-        } else {
-            for (let item of test) {
-                item.classList.remove('hidden')
-            }
-            btnLink.textContent = 'Скрыть'
-            btnImg.classList.remove('change')
-            
-        }
-    
+} else {
+    // В остальных случаях "слушаем" нажатие на кнопку и меняем ее
+    btnLink.addEventListener('click', () => {
+        changeButton()
+    })
+}
+
+window.onresize = () => {
+    // Если происходит ресайс, динамически меняем ширину и включаем свайпер
+    width = window.innerWidth
+
+    if (width < 470) {
+        brand.classList.add('hidden')
+        swipers.classList.remove('hidden')
+
     } else {
-        if (btnLink.textContent === 'Скрыть') {
-            for (let item of test2) {
-                item.style.display = "none"
-            }
-            btnLink.textContent = 'Показать'
-            btnImg.classList.add('change')
-            
-        } else {
-            for (let item of test2) {
-                item.style.display = "flex"
-            }
-            btnLink.textContent = 'Скрыть'
-            btnImg.classList.remove('change')
-
+        brand.classList.remove('hidden')
+        swipers.classList.add('hidden')
         }
-    }
-})
+}
+
+
+
+
+
 
 
         
